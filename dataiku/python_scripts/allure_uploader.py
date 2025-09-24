fc60bf2cba4034ed9cb9f7891846b28419e94ced
Guido Schmutz <schmutz68@gmail.com>
2025-09-24 08:23:25 +0200

    update allure uploader to support pages

--- dataiku/python_scripts/allure_uploader.py
+++ dataiku/python_scripts/allure_uploader.py
@@ -18,36 +18,13 @@ def send_allure_results(
     results_directory = os.path.join(current_directory, allure_results_directory)
     print('RESULTS DIRECTORY PATH: ' + results_directory)
 
-    files = os.listdir(results_directory)
-    print('FILES:')
-    results = []
-    for file in files:
-        result = {}
-        file_path = os.path.join(results_directory, file)
-        print(file_path)
-        if os.path.isfile(file_path):
-            with open(file_path, "rb") as f:
-                content = f.read()
-                if content.strip():
-                    b64_content = base64.b64encode(content)
-                    result['file_name'] = file
-                    result['content_base64'] = b64_content.decode('UTF-8')
-                    results.append(result)
-                else:
-                    print('Empty File skipped: ' + file_path)
-        else:
-            print('Directory skipped: ' + file_path)
-
-    headers = {'Content-type': 'application/json'}
-    request_body = {"results": results}
-    json_request_body = json.dumps(request_body)
-
     print("------------------LOGIN-----------------")
     credentials_body = {
         "username": security_user,
         "password": security_password
     }
     json_credentials_body = json.dumps(credentials_body)
+    headers = {'Content-type': 'application/json'}
 
     session = requests.Session()
     response = session.post(
@@ -66,20 +43,48 @@ def send_allure_results(
         raise Exception("CSRF access token not found in cookies.")
     print("CSRF-ACCESS-TOKEN: " + csrf_access_token)
 
-    print("------------------SEND-RESULTS------------------")
-    headers['X-CSRF-TOKEN'] = csrf_access_token
-    response = session.post(
-        f"{allure_server}/allure-docker-service/send-results?project_id={project_id}&force_project_creation=true",
-        headers=headers,
-        data=json_request_body,
-        verify=ssl_verification
-    )
-    print("STATUS CODE:")
-    print(response.status_code)
-    print("RESPONSE:")
-    print(response.content)
-    json_response_body = json.loads(response.content)
-    print(json.dumps(json_response_body, indent=4, sort_keys=True))
+    files = os.listdir(results_directory)
+    print('FILES:')
+    results = []
+    # Paginate files in blocks of 50
+    block_size = 50
+    for i in range(0, len(files), block_size):
+        block_files = files[i:i + block_size]
+        for file in block_files:
+            result = {}
+            file_path = os.path.join(results_directory, file)
+            print(file_path)
+            if os.path.isfile(file_path):
+                with open(file_path, "rb") as f:
+                    content = f.read()
+                    if content.strip():
+                        b64_content = base64.b64encode(content)
+                        result['file_name'] = file
+                        result['content_base64'] = b64_content.decode('UTF-8')
+                        results.append(result)
+                    else:
+                        print('Empty File skipped: ' + file_path)
+            else:
+                print('Directory skipped: ' + file_path)
+
+        headers = {'Content-type': 'application/json'}
+        request_body = {"results": results}
+        json_request_body = json.dumps(request_body)
+
+        print("------------------SEND-RESULTS------------------")
+        headers['X-CSRF-TOKEN'] = csrf_access_token
+        response = session.post(
+            f"{allure_server}/allure-docker-service/send-results?project_id={project_id}&force_project_creation=true",
+            headers=headers,
+            data=json_request_body,
+            verify=ssl_verification
+        )
+        print("STATUS CODE:")
+        print(response.status_code)
+        print("RESPONSE:")
+        print(response.content)
+        json_response_body = json.loads(response.content)
+        print(json.dumps(json_response_body, indent=4, sort_keys=True))
 
     print("------------------GENERATE-REPORT------------------")
     response = session.get(
