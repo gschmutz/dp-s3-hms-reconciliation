d4e3e01af2daf9df0403021ffcab55bc7e2c35d2
Guido Schmutz <schmutz68@gmail.com>
2025-11-17 11:28:02 +0100

    Update create_baseline_from_hms.py

--- dataiku/python_scripts/hms_compare/create_baseline_from_hms.py
+++ dataiku/python_scripts/hms_compare/create_baseline_from_hms.py
@@ -327,22 +327,22 @@ def generate_baseline_for_table(engine, table: str, schema: str = "public", filt
             create_time_col_agg = ""
             if "PART_ID".lower() in all_columns:
                 create_time_table_join = f"JOIN {catalog_name}public.\"PARTITIONS\" ct ON ct.\"PART_ID\" = t.\"PART_ID\""
-                create_time_col = f', ct."CREATE_TIME" AS create_time'
-                create_time_col_agg = f', COALESCE(MAX(t.create_time), 0) AS max_create_time'
+                create_time_col = f', ct."CREATE_TIME" AS baseline_create_time'
+                create_time_col_agg = f', COALESCE(MAX(t.baseline_create_time), 0) AS max_create_time'
             elif "TBL_ID".lower() in all_columns:
                 create_time_table_join = f"JOIN {catalog_name}public.\"TBLS\" ct ON ct.\"TBL_ID\" = t.\"TBL_ID\""
-                create_time_col = f', ct."CREATE_TIME" AS create_time'
-                create_time_col_agg = f', COALESCE(MAX(t.create_time), 0) AS max_create_time'
+                create_time_col = f', ct."CREATE_TIME" AS baseline_create_time'
+                create_time_col_agg = f', COALESCE(MAX(t.baseline_create_time), 0) AS max_create_time'
             # DBS only has a create_time column in HMS 4.x
             #elif "DB_ID".lower() in all_columns:
             #    create_time_table_join = f"JOIN {catalog_name}public.\"DBS\" ct ON ct.\"DB_ID\" = t.\"DB_ID\""
-            #    create_time_col = f', ct."CREATE_TIME" AS create_time'
-            #    create_time_col_alias = f', MAX(t.create_time) AS max_create_time'
+            #    create_time_col = f', ct."CREATE_TIME" AS baseline_create_time'
+            #    create_time_col_alias = f', MAX(t.baseline_create_time) AS max_create_time'
             # CTLGS only has a create_time column in HMS 4.x
             #elif "CTLGS_ID".lower() in all_columns:
             #    create_time_table_join = f"JOIN {catalog_name}public.\"CTLGS\" ct ON ct.\"CTLGS_ID\" = t.\"CTLGS_ID\""
-            #    create_time_col = f', ct."CREATE_TIME" AS create_time'
-            #    create_time_col_alias = f', MAX(t.create_time) AS max_create_time'
+            #    create_time_col = f', ct."CREATE_TIME" AS baseline_create_time'
+            #    create_time_col_alias = f', MAX(t.baseline_create_time) AS max_create_time'
 
             timestamp_where_clause = ""
             if filter_timestamp and create_time_col:
